ef75a0397927a08a6fffc353022ffb9190d2ed0d
samyak360 <samyak360@gmail.com>
2018-09-15 18:12:29 +0530

    added functionality for windows server

--- remote_client.py
+++ remote_client.py
@@ -7,13 +7,8 @@ s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 # defining ip and port below 
 ip="192.168.1.37"
 port=7890
-#authentication from server
-user = raw_input("enter your username: ")
-password = raw_input("enter your password: ")
-s.sendto(user,(ip,port))
-s.sendto(password,(ip,port))
-msg = s.recvfrom(40)
-if msg[0] == 'ok' :
+#defining functions for windows and linux servers
+def linux():
         # defining list for 10 commands counter
         timer=[]
         while  True :
@@ -36,6 +31,42 @@ if msg[0] == 'ok' :
                                 print "\ncommand not found..\nmake sure you are connected to LINUX server\n"
                         else:
                                 print "\n",recv_cmd,"\n"
+
+def windows():
+        # defining list for 10 commands counter
+        timer=[]
+        while  True :
+        #  sending  data to  target machine 
+                cmd=raw_input("G:\projectpython\server-terminal-python> ")
+                s.sendto(cmd,(ip,port))
+                if  'exit' in  cmd  or  'close' in cmd:
+                        print "closing server.."
+                        exit()
+                else :
+                        timer.append(cmd)
+                        if  len(timer) > 5 :
+                                subprocess.call('cls',shell=True)
+                                for i in  range(len(timer)):
+                                        timer.pop()
+                        server_data=s.recvfrom(500)
+                        #   only  server  data is stored and printed
+                        recv_cmd=server_data[0]
+                        if "not recognized" in recv_cmd :
+                                print "\ncommand not found..\nmake sure you are connected to WINDOWS server\n"
+                        else:
+                                print "\n",recv_cmd,"\n"                                
+#authentication from server
+user = raw_input("enter your username: ")
+password = raw_input("enter your password: ")
+s.sendto(user,(ip,port))
+s.sendto(password,(ip,port))
+msg = s.recvfrom(40)
+if msg[0] == 'ok' :
+        os = subprocess.check_output('nmap -O -V 192.168.1.37',shell=True)
+        if 'windows' in os:
+                windows()
+        else:
+                linux()
 else:
         print "\n",msg[0]
 s.close()
--- remote_server.py
+++ remote_server.py
@@ -22,12 +22,15 @@ if user[0] == 'root' and password[0] == '123' :
         #  only  accepting  commands with  20 char 
                 client_data=s.recvfrom(20)
         #   only  client  data is stored
-                recv_cmd=client_data[0]
+                recvcmd=client_data[0]
         #  executing  client data 
-                if  'exit' in recv_cmd or 'close' in  recv_cmd :
+                if  'exit' in recvcmd or 'close' in  recvcmd :
                         exit()
                 else :
-                        s.sendto(commands.getoutput(recv_cmd),client_data[1])
+                        proc = subprocess.Popen(recvcmd, shell = True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                        out , err = proc.communicate()
+                        s.sendto(out+err,client_data[1])
+                                
 else :
         s.sendto("bad login username or password",user[1])
 s.close()
